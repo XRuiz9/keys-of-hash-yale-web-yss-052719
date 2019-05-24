d29cfe44ecc064fbc15f77d59955179c9a886856
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-24 11:55:52 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,6 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     keys = []
-    class.each do |key, value|
-      if value == arguments
-        keys << key
-      end
-    end
-    keys
+    
   end
 end
